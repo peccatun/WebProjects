1724b4fdabd6446a5fc2025087a0bcd4119dd7f7
Petko <limona.pb@gmail.com>
2020-03-10 15:15:32 +0200

    Commit create comments service and reply view

--- CustomForumAspNetCore/MySecondTryForum/Controllers/CommentsController.cs
+++ CustomForumAspNetCore/MySecondTryForum/Controllers/CommentsController.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace MySecondTryForum.Controllers
 {
     public class CommentsController : Controller
     {
         //TODO: Create comments service
+
+        [HttpGet]
+        public IActionResult AllComents(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Reply(int id)
+        {
+            return this.View();
+        }
     }
 }
