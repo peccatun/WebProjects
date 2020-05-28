f7ff66864be4aee2b80084e9a5ea802862191d02
Petko <limona.pb@gmail.com>
2020-05-28 19:09:58 +0300

    Commit Initial TaskReminder Project

---
+++ TaskReminder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaskReminder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
