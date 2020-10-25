8cefd8b300073138366be04c5c39b0ff7a718c0d
peccatun <petkoboshnakov97@gmail.com>
2020-10-25 09:43:04 +0200

    Commit initial project

---
+++ FurnitureOnlineShop/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FurnitureOnlineShop.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string CategoryName { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public virtual IEnumerable<Product> Products { get; set; } = new HashSet<Product>();
+    }
+}
---
+++ FurnitureOnlineShop/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FurnitureOnlineShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ProductName { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string[] AdditionalImageUrls { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
