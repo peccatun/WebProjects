3de48dbcc0b898419b4f741aee7981953d058cc5
peccatun <limona.pb@gmail.com>
2021-12-03 08:12:10 +0200

    Maintenance controller initial

---
+++ MotMainOnline/MotMaintOnline4/Controllers/MaintenanceController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MotMaintOnline4.Controllers
+{
+    public class MaintenanceController : Controller
+    { 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
