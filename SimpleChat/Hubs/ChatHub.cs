0feff40c3335a5945d3741642b0f2320b7692e24
Petko <limona.pb@gmail.com>
2021-12-19 17:07:27 +0200

    Initial project full functionality

---
+++ SimpleChat/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using ChatApplication.Models.Chat;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApplication.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message) 
+        {
+            await this.Clients.All.SendAsync(
+                "NewMessage",
+                new Message 
+                { User = this.Context.User.Identity.Name, Text = message  }
+                );   
+        }
+    }
+}
---
+++ SimpleChat/Models/Chat/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatApplication.Models.Chat
+{
+    public class Message
+    {
+        public string User { get; set; }
+
+        public string Text { get; set; }
+    }
+}
